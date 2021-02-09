94337d826b820948af4735b431e3b3d075b22144
Subin <psb4644@gmail.com>
2021-02-09 22:42:18 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let a = 23; //let은 변수가 바뀌는걸 허용
+const b = 23; //const는 변수가 바뀌는걸 허용하지 않음
+var c = 23; /*var는 변수가 바뀌는걸 허용 하지만
+var name = 'bathingape'
+console.log(name) // bathingape
+
+var name = 'javascript'
+console.log(name) // javascript
+과 같이 에러가 나오지 않고 각기 다른 값이 출력된다.
+즉 코드량이 많아진다면 값이 바뀔 우려가 있다.
+*/
+console.log(a);
