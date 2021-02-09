c0030f0a92969a840c923ad92219946c31501c11
Subin <psb4644@gmail.com>
2021-02-09 23:31:27 +0900

    Camel case or Arry

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-let a = 23; //let은 변수가 바뀌는걸 허용 무조건 필요 할 때 사용하는걸 추천
-const b = 23; //const는 변수가 바뀌는걸 허용하지 않음 대부분 이걸 씀
-var c = 23; 
-/*var는 변수가 바뀌는걸 허용 하지만
-
-var name = 'bathingape'
-console.log(name) // bathingape
-var name = 'javascript'
-console.log(name) // javascript
-
-과 같이 에러가 나오지 않고 각기 다른 값이 출력된다.
-즉 코드량이 많아진다면 값이 바뀔 우려가 있다.
-console.log(a);
+/* Camel case
+변수명을 소문자로 시작해서
+스페이스가 필요하면 대문자로 대체
 */
-/* String
-const what = "Subin";
-alert(what);
-*/
-
-/*Boolean
-const what = true;
-*/
-
-/* Flaot
-const what = 55.1;
-*/
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "fri", "Sat", "Sun"];
+alert(daysOfWeek[2]);
