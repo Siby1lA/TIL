c1e8f7908e7fa2858dc24b5676bf2bf06b9d2444
Subin <psb4644@gmail.com>
2021-02-12 16:52:52 +0900

    DOM if else function practice

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const age = prompt("How old are you");
+const title = document.querySelector("#title");
 
-if(age>=18 && age<=21){
-    console.log('you can drink but you should');
-}else if(age > 21){
-    console.log('go ahed');
-}else{
-    console.log('too young')
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick(){
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    }else{
+        title.style.color = BASE_COLOR;
+    }
 }
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+init();
