9dded16feb39c7589a82977f798f8bf5010d31a5
Subin <psb4644@gmail.com>
2021-02-22 05:51:28 +0900

    Saving the User Name part One

---
+++ js/greeting.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('.js-form');
+const input = form.querySelector('input');
+const greeting = document.querySelector('.js-greetings');
+
+const USER_LS = "currentUser"
+const SHOWING_CN = "showing";
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+     if(currentUser === null){
+
+     }else{
+        paintGreeting(currentUser);
+     }
+}
+
+function init(){
+    loadName();
+}
+
+init();
