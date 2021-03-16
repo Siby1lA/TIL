334c76636ca5762b557b8d1fb25a8c84fcb53caa
Subin <psb4644@gmail.com>
2021-03-16 21:48:55 +0900

    update

--- iframe-youtube/index.js
+++ iframe-youtube/index.js
@@ -1,4 +1,5 @@
 const Btn = document.querySelector(".btn1");
+const Btn2 = document.querySelector(".btn2");
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('trailer-movies__movie', {
@@ -11,8 +12,13 @@ function change() {
     player.loadVideoById('1Nv-vPBA0fI');
 }
 
+function change2() {
+    player.loadVideoById('v7BY5m2wYx4');
+}
+
 function init(){
     Btn.addEventListener('click', change);
+    Btn2.addEventListener('click', change2);
 }
 
 init();
