e3e666eb77bf554bdd56560e37d78244600a92db
Subin <psb4644@gmail.com>
2021-07-13 18:31:04 +0900

    Image require

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 import GameScreen from './GameScreen';
+
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Game is Over!</Text>
+            <Image source={require('../assets/success.png')}/>
             <Text>Number of rounds: {props.roundsNumber} </Text>
             <Text>Number was: {props.userNumber} </Text>
             <Button title="Re Start" onPress={props.onRestart} />
