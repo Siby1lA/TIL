52526f43bd7fe3a0069aa29c9ab27e054ca075bc
Siby1lA <psb4644@gmail.com>
2022-02-28 18:45:25 +0900

    useClick

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, [onClick]);
+
+  return element;
+};
 const App = () => {
-  const input = useRef(); //getElementById()와 같다.
-  setTimeout(() => input.current.focus(), 5000);
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
   return (
     <div>
-      <div>hi</div>
-      <input ref={input} placeholder="la" />
+      <h1 ref={title}>hi</h1>
     </div>
   );
 };
