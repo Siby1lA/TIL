2f5094ddd4b5ccde78242adcf26f8cf9b45c4e7a
Siby1lA <psb4644@gmail.com>
2022-03-02 13:31:16 +0900

    useNotification

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
-import { useRef } from "react";
-
-const useFullScreen = (callBack) => {
-  const element = useRef();
-  const runCallBack = (isFull) => {
-    if (callBack && typeof callBack === "function") {
-      callBack(isFull);
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
     }
   };
-  const triggerFull = () => {
-    if (element.current) {
-      element.current.requestFullscreen();
-      runCallBack(true);
-    }
-  };
-  const exitFull = () => {
-    document.exitFullscreen();
-    if (callBack && typeof callBack === "function") {
-      runCallBack(false);
-    }
-  };
-  return { element, triggerFull, exitFull };
+  return fireNotif;
 };
-
 const App = () => {
-  const onFull = (isFull) => {
-    console.log(isFull ? "We are full" : "we are small");
-  };
-  const { element, triggerFull, exitFull } = useFullScreen(onFull);
+  const trigerNofitif = useNotification("Can I steal your kimchi?", {
+    body: "I love kimchi",
+  });
   return (
     <div>
-      <div ref={element}>
-        <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="grape" width="250" />
-        <button onClick={triggerFull}>Make fullscreen</button>
-        <button onClick={exitFull}>Exit fullscreen</button>
-      </div>
+      <button onClick={trigerNofitif}>Hello</button>
     </div>
   );
 };
