64eccbf20078536681a663446e705879f82a8c95
Siby1lA <psb4644@gmail.com>
2022-03-04 01:24:00 +0900

    2.6 Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,29 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import {
+  HashRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 const AppRouter = (props) => {
   return (
     <Router>
+      {props.isLoggedIn && <Navigation />}
       <Routes>
         {props.isLoggedIn ? (
           <>
-            <Route exact={true} path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
           </>
         ) : (
-          <Route exact={true} path="/" element={<Auth />} />
+          <>
+            <Route path="/" element={<Auth />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
         )}
       </Routes>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,13 @@
+import { authService } from "fbase";
 import React from "react";
 const Profile = () => {
-  return <span>Profile</span>;
+  const onLogoutClick = () => {
+    authService.signOut();
+  };
+  return (
+    <>
+      <button onClick={onLogoutClick}>Log out</button>
+    </>
+  );
 };
 export default Profile;
