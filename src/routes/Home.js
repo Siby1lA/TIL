32f77e12deb4319b5e3b17546aa3ec41201d86a9
Siby1lA <psb4644@gmail.com>
2022-03-05 09:13:10 +0900

    4.1 Preview Images part Two

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -59,7 +59,7 @@ const Home = (props) => {
     };
     reader.readAsDataURL(theFile);
   };
-  const onClearPhotoClick = () => setAttachment(null);
+  const onClearAttachment = () => setAttachment(null);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -75,7 +75,7 @@ const Home = (props) => {
         {attachment && (
           <div>
             <img src={attachment} width="50px" height="50px" />
-            <button onClick={onClearPhotoClick}>Clear</button>
+            <button onClick={onClearAttachment}>Clear</button>
           </div>
         )}
       </form>
