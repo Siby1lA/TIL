e9d473ba7ed266ac1f6342e54ea6adfc6c272c6d
Siby1lA <psb4644@gmail.com>
2022-03-16 23:56:14 +0900

    redux-logger & midddleware with DevTools

--- React/react-middleware/src/index.js
+++ React/react-middleware/src/index.js
@@ -4,8 +4,14 @@ import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
-import myLogger from "./middlewares/myLogger";
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+); // 여러개의 미들웨어를 적용 할 수 있습니다.
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
