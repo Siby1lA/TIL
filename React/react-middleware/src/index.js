fc54b0e64ec67cfac48106bf381b796ef3eafb4c
Siby1lA <psb4644@gmail.com>
2022-03-17 11:23:53 +0900

    redux-thunk

--- React/react-middleware/src/index.js
+++ React/react-middleware/src/index.js
@@ -6,10 +6,10 @@ import { Provider } from "react-redux";
 import rootReducer from "./modules";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
-
+import ReduxThunk from "redux-thunk";
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
 ); // 여러개의 미들웨어를 적용 할 수 있습니다.
 
 ReactDOM.render(
