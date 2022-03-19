2cdabb5fee1936a96b5fa96ad57d5c72dfeee5ae
Siby1lA <psb4644@gmail.com>
2022-03-19 10:27:05 +0900

    styled-components 'As'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
+
 const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
+
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Btn>Log in</Btn>
+      <Btn as="a">Log in</Btn>
     </Father>
   );
 }
