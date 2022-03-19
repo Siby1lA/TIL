3639a56cd6d1d0111f3323443fb756081b56de05
Siby1lA <psb4644@gmail.com>
2022-03-19 13:48:03 +0900

    Themes

--- src/App.js
+++ src/App.js
@@ -6,45 +6,17 @@ const Wrapper = styled.div`
   height: 100vh;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const rotationAnimation = keyframes`
- 0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    border-radius:100px;
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>Hi</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
