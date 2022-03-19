e21f95b7a18ab81da90bc5d5ceddd8b4c527f634
Siby1lA <psb4644@gmail.com>
2022-03-20 08:43:29 +0900

    Optional Props

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Circle borderColor="yellow" bgColor="teal" />
+      <Circle text="hello" bgColor="tomato" />
     </div>
   );
 }
