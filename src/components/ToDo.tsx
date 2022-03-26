f55c3e016996975794e8ce5cea330342ecee361c
Siby1lA <psb4644@gmail.com>
2022-03-26 22:06:17 +0900

    Refactoring

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,13 @@
+import { IToDo } from "../atoms";
+
+function ToDo({ text }: IToDo) {
+  return (
+    <li>
+      <span>{text}</span>
+      <button>To Do</button>
+      <button>Doing</button>
+      <button>Done</button>
+    </li>
+  );
+}
+export default ToDo;
