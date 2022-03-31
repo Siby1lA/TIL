af0dd5443e1f0cd283d31b19efb7287bd7bd13b5
Siby1lA <psb4644@gmail.com>
2022-03-31 19:26:59 +0900

    Performance

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
+import DragabbleCard from "./Components/DragbbleCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -30,13 +31,6 @@ const Board = styled.div`
   min-height: 200px;
 `;
 
-const Card = styled.div`
-  border-radius: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
-  margin-bottom: 5px;
-`;
-
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
@@ -58,17 +52,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable key={toDo} draggableId={toDo} index={index}>
-                    {(magic) => (
-                      <Card
-                        ref={magic.innerRef}
-                        {...magic.draggableProps}
-                        {...magic.dragHandleProps}
-                      >
-                        {toDo}
-                      </Card>
-                    )}
-                  </Draggable>
+                  <DragabbleCard key={toDo} index={index} toDo={toDo} />
                 ))}
                 {magic.placeholder}
               </Board>
---
+++ src/Components/DragbbleCard.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Card = styled.div`
+  border-radius: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+  margin-bottom: 5px;
+`;
+
+interface IDragabbleCardProps {
+  toDo: string;
+  index: number;
+}
+
+function DragabbleCard({ toDo, index }: IDragabbleCardProps) {
+  return (
+    <Draggable key={toDo} draggableId={toDo} index={index}>
+      {(magic) => (
+        <Card
+          ref={magic.innerRef}
+          {...magic.draggableProps}
+          {...magic.dragHandleProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+export default React.memo(DragabbleCard);
