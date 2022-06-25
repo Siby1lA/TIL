af910114b2c4a46945e193be6208a8185bbf4b32
Siby1lA <psb4644@gmail.com>
2022-06-25 14:45:10 +0900

    클라이언트 서버 연동 프록시 설정

---
+++ boilerPlate/client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import Registerpage from "./components/views/RegisterPage/registerPage";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<Registerpage />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- boilerPlate/server/index.js
+++ boilerPlate/server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
@@ -21,6 +21,10 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/api/hello", (req, res) => {
+  res.send("hi");
 });
 
 app.post("/api/users/register", (req, res) => {
@@ -90,6 +94,7 @@ app.get("/api/users/logout", auth, (req, res) => {
   });
 });
 
+const port = 5000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
