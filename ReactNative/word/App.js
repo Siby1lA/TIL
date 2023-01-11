16e6d6e2aa1182bcd215f1f9853fffe7347ded85
Siby1lA <psb4644@gmail.com>
2023-01-11 23:30:37 +0900

    feat: animation

---
+++ ReactNative/word/App.js
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from "react";
+import { Animated, Easing, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
+export default function App() {
+  const [up, setUp] = useState(false);
+  const Y = useRef(new Animated.Value(0)).current;
+  const toggleUp = () => setUp((prev) => !prev);
+  const moveUp = () => {
+    Animated.timing(Y, {
+      toValue: up ? 200 : -200,
+      useNativeDriver: true,
+      easing: Easing.circle,
+    }).start(toggleUp);
+  };
+  return (
+    <Container>
+      <TouchableOpacity onPress={moveUp}>
+        <AnimatedBox
+          style={{
+            transform: [{ translateY: Y }],
+          }}
+        />
+      </TouchableOpacity>
+    </Container>
+  );
+}
