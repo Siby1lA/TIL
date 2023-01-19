804f7eda9269733d40f7a0e6d14278717bb39ae8
Siby1lA <psb4644@gmail.com>
2023-01-19 23:09:32 +0900

    server side rendering

--- NextJS/nextjs-intro/src/pages/index.tsx
+++ NextJS/nextjs-intro/src/pages/index.tsx
@@ -1,19 +1,11 @@
 import { useEffect, useState } from "react";
-import Seo from "../components/HeaderBar";
+import HeaderBar from "../components/HeaderBar";
 
-export default function Home() {
-  const [movies, setMovies] = useState<any>([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
-      <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie: any) => (
+      <HeaderBar title="Home" />
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -42,4 +34,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
