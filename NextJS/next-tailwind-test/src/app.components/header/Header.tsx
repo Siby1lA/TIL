4148a88098bfce5fae79dc16571ec2fa81a45208
Siby1lA <psb4644@gmail.com>
2023-01-21 13:08:03 +0900

    tailwind 연습 헤더 정렬

--- NextJS/next-tailwind-test/src/app.components/header/Header.tsx
+++ NextJS/next-tailwind-test/src/app.components/header/Header.tsx
@@ -1,9 +1,9 @@
 function Header() {
 	return (
 		<div className="flex justify-between items-center p-3">
-			<div className="text-slate-50">공백</div>
+			<div className="w-[6%]"></div>
 			<div className="text-gray-500">00편의점 00지점</div>
-			<div>알람</div>
+			<div className="w-[6%]">알림</div>
 		</div>
 	);
 }
